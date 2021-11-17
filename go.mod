e33e2fbbc942004edbc41e7141f6366dfb107d4b
jadahbakar <dedy.styawan@gmail.com>
2021-11-17 09:25:53 +0700

    still on csr

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module crawl
 go 1.15
 
 require (
-	github.com/gocolly/colly/v2 v2.1.0
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/geziyor/geziyor v0.0.0-20211021191925-369b42cbc6c5
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/net v0.0.0-20211116231205-47ca1ff31462 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,43 +1,21 @@
 package main
 
 import (
-	"log"
-	"strings"
-	"time"
+	"fmt"
 
-	"github.com/gocolly/colly/v2"
-	"github.com/gocolly/colly/v2/debug"
+	"github.com/geziyor/geziyor"
+	"github.com/geziyor/geziyor/client"
 )
 
-type Link struct {
-	url   string
-	title string
-}
-
-func CrawlLink(url string) []Link {
-	var result []Link
-
-	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
-		colly.Async(),
-		colly.Debugger(&debug.LogDebugger{}),
-	)
-
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		time.Sleep(10 * time.Second)
-		link := e.Attr("href")
-		str := strings.TrimSpace(e.Text)
-		result = append(result, Link{url: link, title: str})
-	})
-
-	c.Visit(url)
-	c.Wait()
-
-	return result
-}
-
 func main() {
-	// data := CrawlLink("https://ssr-assessment.miqdadyyy.vercel.app/")
-	data := CrawlLink("https://csr-assessment.miqdadyyy.vercel.app/")
-	log.Println(data)
+
+	geziyor.NewGeziyor(&geziyor.Options{
+		StartRequestsFunc: func(g *geziyor.Geziyor) {
+			g.GetRendered("https://csr-assessment.miqdadyyy.vercel.app/", g.Opt.ParseFunc)
+		},
+		ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+			fmt.Println(string(r.Body))
+		},
+		// BrowserEndpoint: "ws://localhost:3000",
+	}).Start()
 }
